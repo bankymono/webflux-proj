c6e394da821ab6b2a5b542b1f38ac0c01e101aa8
Ayodeji Bankole <bankymono@gmail.com>
2026-01-19 20:12:59 +0100

    #

--- src/main/java/com/bankymono/webflux_proj/sec05/filter/AuthenticationWebFilter.java
+++ src/main/java/com/bankymono/webflux_proj/sec05/filter/AuthenticationWebFilter.java
@@ -1,5 +1,7 @@
 package com.bankymono.webflux_proj.sec05.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerWebExchange;
@@ -10,8 +12,12 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 import java.util.Objects;
 
+@Order(1)
 @Service
 public class AuthenticationWebFilter implements WebFilter {
+
+    @Autowired
+    private FilterErrorHandler filterErrorHandler;
 
     private static final Map<String, Category> TOKEN_CATEGORY_MAP = Map.of(
             "secret123",Category.STANDARD,
@@ -23,9 +29,12 @@ public class AuthenticationWebFilter implements WebFilter {
         var token = exchange.getRequest().getHeaders().getFirst("auth-token");
 
         if(Objects.nonNull(token) && TOKEN_CATEGORY_MAP.containsKey(token)) {
+            exchange.getAttributes().put("category", TOKEN_CATEGORY_MAP.get(token));
             return chain.filter(exchange);
         }
 
-        return Mono.fromRunnable(() -> exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED));
+//        return Mono.fromRunnable(() -> exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED));
+
+        return filterErrorHandler.sendProblemDetail(exchange, HttpStatus.UNAUTHORIZED, "Set the valid token");
     }
 }
---
+++ src/main/java/com/bankymono/webflux_proj/sec05/filter/AuthorizationWebFilter.java
@@ -0,0 +1,39 @@
+package com.bankymono.webflux_proj.sec05.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Order(2)
+@Service
+public class AuthorizationWebFilter implements WebFilter {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        var category = exchange.getAttributeOrDefault("category", Category.STANDARD);
+        return switch (category) {
+            case PRIME -> prime(exchange,chain);
+            case STANDARD -> standard(exchange,chain);
+        };
+    }
+
+    private Mono<Void> prime(ServerWebExchange exchange, WebFilterChain chain) {
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> standard(ServerWebExchange exchange, WebFilterChain chain) {
+        var isGet = HttpMethod.GET.equals(exchange.getRequest().getMethod());
+        if(isGet) {
+            return chain.filter(exchange);
+        }
+        return Mono.fromRunnable(() -> exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN));
+
+    }
+}
